8358d1a9ce877efdbc74ca0877ed25ee41818f9a
bo-yoon <bykwon223@gmail.com>
2021-11-03 00:10:15 +0900

    add

---
+++ product/src/main/java/com/springcloud/product/common/controller/ConfigClient.java
@@ -0,0 +1,17 @@
+package com.springcloud.product.common.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClient {
+
+    @Value("${config.name}")
+    private String profile;
+
+    @GetMapping( value = "/config/profile", produces = "application/json; charset=UTF8")
+    public String configConnection() {
+         return profile;
+    }
+}
